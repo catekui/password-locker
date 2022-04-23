a51e6185148ad2df1e7495569d3d13b6399daae5
catekui <catherine.wanjiku@student.moringaschool.com>
2022-04-23 21:37:17 +0300

    add methods for create saving users

--- user.py
+++ user.py
@@ -5,14 +5,22 @@ class User:
 
     pass
 #init method to create new instances of a class
-    def __init__(self,first_name,last_name,username, password):
+    user_list = []
+    def __init__(self,first_name,last_name,username,password):
+
+    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+
+    user_list = [] # Empty contact list
+ # Init method up here
+    def save_user(self):
 
         '''
-        __init__ method that helps us define properties for our objects.
-
-        Args:
-            first_name: New contact first name.
-            last_name : New contact last name.
-            username: New contact username.
-            password : New contact password.
+        save_user method saves user objects into user_list
         '''
+
+        User.user_list.append(self)
