bcef87e54921e51275d05f7dc756dd8d23b43656
catekui <catherine.wanjiku@student.moringaschool.com>
2022-04-23 21:06:09 +0300

    create an init method for the class

---
+++ user.py
@@ -0,0 +1,18 @@
+class User:
+    """
+    Class that generates new instances of users
+    """
+
+    pass
+#init method to create new instances of a class
+    def __init__(self,first_name,last_name,username, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New contact first name.
+            last_name : New contact last name.
+            username: New contact username.
+            password : New contact password.
+        '''
