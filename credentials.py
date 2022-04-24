355077a7205e4c28060e94b25a30b1f241db422b
catekui <catherine.wanjiku@student.moringaschool.com>
2022-04-24 15:49:29 +0300

    create save and delete methods

--- credentials.py
+++ credentials.py
@@ -11,5 +11,19 @@ class Credentials:
         self.accountusername = accountusername
         self.accountname = accountname
         self.accountpassword = accountpassword
+        #saving method
+    def save_account(self):
+        '''
+        save_account method saves user info into accounts
+        '''
+
+        Credentials.accounts.append(self)
+
+    def delete_account(self):
+        '''
+        delete_account method deletes user info from the accounts
+        '''
+
+        credentials.accounts.remove(self)
     
         
